a678c5d77bf30b11d0cf37f151034dd3d2c6f94a
Michael <michaeltduvall@gmail.com>
2016-11-04 00:20:37 -0700

    Commented on how the challenge was solved to help solidify my understanding

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -10,3 +10,17 @@ class Complement
     sequence.gsub(/[#{dna_nucleotides}]/, 'A'=>'U', 'C'=>'G', 'G'=>'C', 'T'=>'A')
   end
 end
+
+=begin
+This program asks us to essentially swap chars out from a string (DNA => RNA)
+
+For this task, we set a string for the 'dna_nucleotides'
+
+Next, we will return '' if the regexp does not fully #match the sequence
+/ / = regular expression
+[ ] = sort through every char in the regular expression
+^ = 'not'
+
+The next line, we utilize the String#gsub method, a hash and regular expression to
+sort through each letter and swap the DNA nucleotide value with the RNA nucleotide value.
+=end
