cd324be444fb3d3bdde7d8c60f0db31eb7549eb8
Michael <michaeltduvall@gmail.com>
2016-11-03 23:34:41 -0700

    Utilized RegExp to swap nucleotides (chars) and verify arbitrary input

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,11 +1,12 @@
-class Complement
+module BookKeeping
+VERSION = 4
+end
 
+class Complement  
   def self.of_dna(sequence)
     dna_nucleotides = 'ACGT'
-    if sequence.each_char {|c| /#{c}/.match(dna_nucleotides) } 
-      sequence.gsub(/[#{dna_nucleotides}]/, 'A'=>'U', 'C'=>'G', 'G'=>'C', 'T'=>'A')
-    else
-      return "x"
-    end
+    
+    return '' if /[^#{dna_nucleotides}]/.match(sequence)
+    sequence.gsub(/[#{dna_nucleotides}]/, 'A'=>'U', 'C'=>'G', 'G'=>'C', 'T'=>'A')
   end
 end
