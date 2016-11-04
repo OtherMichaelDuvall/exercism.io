e14b4046763b85e3bdedcde20e3d137eb759daed
Michael <michaeltduvall@gmail.com>
2016-11-04 01:11:52 -0700

    Simplified the last line of Complement#of_dna by calling String#tr to swap the characters in the DNA strand

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -7,20 +7,6 @@ class Complement
     dna_nucleotides = 'ACGT'
     
     return '' if /[^#{dna_nucleotides}]/.match(sequence)
-    sequence.gsub(/[#{dna_nucleotides}]/, 'A'=>'U', 'C'=>'G', 'G'=>'C', 'T'=>'A')
+    sequence.tr('ACGT', 'UGCA')
   end
 end
-
-=begin
-This program asks us to essentially swap chars out from a string (DNA => RNA)
-
-For this task, we set a string for the 'dna_nucleotides'
-
-Next, we will return '' if the regexp does not fully #match the sequence
-/ / = regular expression
-[ ] = sort through every char in the regular expression
-^ = 'not'
-
-The next line, we utilize the String#gsub method, a hash and regular expression to
-sort through each letter and swap the DNA nucleotide value with the RNA nucleotide value.
-=end
