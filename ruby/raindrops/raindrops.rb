f9489d8a9f9c33fb57650c6f67ed406b472b723a
Michael <michaeltduvall@gmail.com>
2016-11-07 23:17:49 -0800

    Got rid of arrays, made use of Hash and Enumerable

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,18 +2,12 @@ module BookKeeping
 VERSION = 3
 end
 
-class Raindrops
-  def self.convert(natural_number)
-    raindrop = { 3 => "Pling", 5 => "Plang", 7 => "Plong" } 
-    divisor  = [3, 5, 7]
-    counted_drops ||= []
- 
-    divisor.each do |divisor_element|
-      remainder = natural_number % divisor_element
-      counted_drops << raindrop[divisor_element] if remainder.zero? 
-    end
-    
-    (counted_drops.empty?) ? natural_number.to_s : counted_drops.join
+module Raindrops
+  def self.convert(integer)
+    rain_hash = { 3 => "Pling", 5 => "Plang", 7 => "Plong" } 
+  
+    drop_count = rain_hash.select { |key| integer % key == 0 }
+    drop_count.empty? ? integer.to_s : drop_count.values.reduce(:+)
   end
 end
 
