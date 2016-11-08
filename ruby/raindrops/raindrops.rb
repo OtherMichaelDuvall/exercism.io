0772415fc561f4d70f74a19e661fe5e84c3ee66c
Michael <michaeltduvall@gmail.com>
2016-11-07 22:54:33 -0800

    PlingPlangPlong

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,6 +1,19 @@
+module BookKeeping
+VERSION = 3
+end
+
 class Raindrops
-
-  def convert(natural_number)
-
+  def self.convert(natural_number)
+    raindrop = { 3 => "Pling", 5 => "Plang", 7 => "Plong" } 
+    divisor  = [3, 5, 7]
+    counted_drops ||= []
+ 
+    divisor.each do |divisor_element|
+      remainder = natural_number % divisor_element
+      counted_drops << raindrop[divisor_element] if remainder.zero? 
+    end
+    
+    (counted_drops.empty?) ? natural_number.to_s : counted_drops.join
   end
 end
+
