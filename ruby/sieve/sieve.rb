4a5ac4467e5a1474a103ede6e8a96918403a997f
Michael <michaeltduvall@gmail.com>
2016-11-15 16:51:06 -0800

    Need to find a better algorithm; too much hard coding

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,9 +1,24 @@
-class Sieve
-  def initialize(range_limit)
-    @prime_numbers = [*1..range_limit]
+module BookKeeping
+VERSION = 1
+end
+
+class Sieve 
+  def initialize(number_input)
+    @start_at = [4, 6, 10, 14, 22, 26]
+    @interval = [2, 3, 5, 7, 11, 13]
+    @number   = number_input
   end
 
   def primes
-    while(@prime_numbers:
+    not_prime ||= []
+    index = 0
+
+    @interval.each do |interval|
+      (@start_at[index]..@number).step(interval) { |x| not_prime << x }
+      index += 1
+    end
+    
+    primes = [*2..@number] - not_prime
   end
+
 end
